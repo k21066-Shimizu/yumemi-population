bb29a84b7e4ff0c303366fc1bc29d614c2e03cc5
k21066-Shimizu <k21066kk@aitech.ac.jp>
2024-03-24 17:57:21 +0900

    refactor: console.logの消し忘れ

--- src/foundations/resasApi.ts
+++ src/foundations/resasApi.ts
@@ -7,7 +7,7 @@ const ENDPOINT = 'https://opendata.resas-portal.go.jp/';
 export const fetchResasApi: FetchResasApi = async (path, params = {}) => {
   const url = new URL(path, ENDPOINT);
   Object.entries(params).forEach(([key, value]) => url.searchParams.append(key, String(value)));
-  console.log(url);
+
   const response = await fetch(url, {
     headers: {
       'Content-Type': 'application/json',
